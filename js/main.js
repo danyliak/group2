6917ad5c0a64e17471233e391c257baf38ac2b81
xalumok <yurkok12@gmail.com>
2017-09-19 20:14:37 +0300

    news letter added

--- js/main.js
+++ js/main.js
@@ -3,7 +3,26 @@
 $(function(){
     'use strict';
 
-    $('#header').load('header.html');
+    $('#header').load('header.html', function () {
+        function renewItemsCnt (){
+            var cartIcon = $(".sh-cart-icon");
+            var itemsCnt = $(".sh-items-cnt");
+            if (itemsCnt.attr("data-items-cnt") !== "0"){
+                console.log("show");
+                itemsCnt.removeClass("sh-cart-empty");
+            }
+            else{
+                itemsCnt.addClass("sh-cart-empty");
+            }
+        }
+        renewItemsCnt();
+
+        $(".shtorka").on("click", function () {
+            $(".mobile-menu").slideToggle();
+            $(".shtorka .fa-times").toggleClass("hidden");
+            $(".shtorka .fa-angle-down").toggleClass("hidden");
+        });
+    });
     $('#footer').load('footer.html');
 
 });
