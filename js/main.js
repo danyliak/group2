4b1d10181e76ed4a8846873cc2402cf48cfc4c6e
TomkivO <olegtomkiv10@gmail.com>
2017-09-21 19:58:07 +0300

    products left side

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-
-
-$(function(){
+$(function () {
     'use strict';
 
     $('#header').load('header.html', function () {
-        function renewItemsCnt (){
+        function renewItemsCnt() {
             var cartIcon = $(".sh-cart-icon");
             var itemsCnt = $(".sh-items-cnt");
-            if (itemsCnt.attr("data-items-cnt") !== "0"){
+            if (itemsCnt.attr("data-items-cnt") !== "0") {
                 console.log("show");
                 itemsCnt.removeClass("sh-cart-empty");
             }
-            else{
+            else {
                 itemsCnt.addClass("sh-cart-empty");
             }
         }
+
         renewItemsCnt();
 
         $(".shtorka").on("click", function () {
@@ -24,5 +23,52 @@ $(function(){
         });
     });
     $('#footer').load('footer.html');
+
+    $('label').click(function () {
+        if ($('input', this).is(':checked')) {
+            $(this).css('color', '#ff5912');
+        } else {
+            $(this).css('color', '#34404b');
+        }
+    });
+
+    $("#slider").slider({
+        min: 0,
+        max: 1000,
+        values: [0, 1000],
+        range: true,
+        stop: function (event, ui) {
+            $("input#minCost").val($("#slider").slider("values", 0));
+            $("input#maxCost").val($("#slider").slider("values", 1));
+        },
+        slide: function (event, ui) {
+            $("input#minCost").val($("#slider").slider("values", 0));
+            $("input#maxCost").val($("#slider").slider("values", 1));
+        }
+    });
+
+    $("input#minCost").change(function () {
+        var value1 = $("input#minCost").val();
+        var value2 = $("input#maxCost").val();
+        if (parseInt(value1) > parseInt(value2)) {
+            value1 = value2;
+            $("input#minCost").val(value1);
+        }
+        $("#slider").slider("values", 0, value1);
+    });
+    $("input#maxCost").change(function () {
+        var value1 = $("input#minCost").val();
+        var value2 = $("input#maxCost").val();
+        if (value2 > 1000) {
+            value2 = 1000;
+            jQuery("input#maxCost").val(1000)
+        }
+        if (parseInt(value1) > parseInt(value2)) {
+            value2 = value1;
+            $("input#maxCost").val(value2);
+        }
+
+
+    });
 
 });
