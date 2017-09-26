dcb1ad3869147c65f837387241ce1953c67c980e
TomkivO <olegtomkiv10@gmail.com>
2017-09-26 15:34:06 +0300

    Title change

--- js/main.js
+++ js/main.js
@@ -178,6 +178,18 @@ $(function () {
             $('#quantity').val(quantity - 1);
         }
     });
+    function expand() {
+        $(".search").toggleClass("close");
+        $(".input").toggleClass("square");
+        if ($('.search').hasClass('close')) {
+            $('input').focus();
+        } else {
+            $('input').blur();
+        }
+    }
+    $('button').on('click', expand);
+
+    copyright('');
 
 
 });
